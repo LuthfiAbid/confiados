06557f00e0ab2d13f040c4a7eeac34150b0b38a7
Pepe <rizal27pangestu@gmail.com>
2019-07-31 09:16:01 +0700

    .

--- app/src/main/java/com/abid/confiados/ProfileActivity.kt
+++ app/src/main/java/com/abid/confiados/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.abid.confiados
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile.*
 
@@ -15,6 +16,8 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
         fAuth = FirebaseAuth.getInstance()
         pref = Preferences(this)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         btn_logout.setOnClickListener {
             pref.setStatus(false)
             fAuth.signOut()
