5f413d45eb480608a5f92e4bda82aae21831ae54
Pepe <rizal27pangestu@gmail.com>
2019-08-14 14:43:01 +0700

    Show Confiados on Cart

--- app/src/main/java/com/abid/confiados/fragment/CartFragment.kt
+++ app/src/main/java/com/abid/confiados/fragment/CartFragment.kt
@@ -3,13 +3,31 @@ package com.abid.confiados.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.abid.confiados.R
+import com.abid.confiados.adapter.ConfiadosAdapter
+import com.abid.confiados.adapter.DestinationAdapter
+import com.abid.confiados.data.Pref
+import com.abid.confiados.model.ConfiadosModel
+import com.abid.confiados.model.DestinationModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import java.util.ArrayList
 
 class CartFragment : Fragment() {
+
+    private lateinit var fAuth: FirebaseAuth
+    private var confiadosAdapter: ConfiadosAdapter? = null
+    private var recyclerView: RecyclerView? = null
+    private var list: MutableList<ConfiadosModel> = ArrayList()
+    lateinit var dbRef: DatabaseReference
+    lateinit var pref: Pref
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,5 +44,37 @@ class CartFragment : Fragment() {
             fragment.arguments = args
             return fragment
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        pref = Pref(context!!)
+        var linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        recyclerView = view.findViewById(R.id.recyclerViewCart)
+        recyclerView!!.layoutManager = linearLayoutManager
+        recyclerView!!.setHasFixedSize(true)
+        fAuth = FirebaseAuth.getInstance()
+        dbRef = FirebaseDatabase.getInstance()
+            .getReference("dataConfiados")
+        dbRef.orderByChild("idusertitip").equalTo(fAuth.uid).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(data: DataSnapshot) {
+                list = ArrayList()
+                for (dataSnapshot in data.children) {
+                    val addDataAll =
+                        dataSnapshot.getValue(ConfiadosModel::class.java)
+                    addDataAll!!.key = dataSnapshot.key
+                    list.add(addDataAll)
+                    confiadosAdapter = ConfiadosAdapter(context!!, list)
+                    recyclerView!!.adapter = confiadosAdapter
+                }
+            }
+
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.e(
+                    "TAG_ERROR", p0.message
+                )
+            }
+        })
     }
 }
--- app/src/main/java/com/abid/confiados/model/ConfiadosModel.kt
+++ app/src/main/java/com/abid/confiados/model/ConfiadosModel.kt
@@ -2,7 +2,9 @@ package com.abid.confiados.model
 
 class ConfiadosModel(
     var key: String? = null,
+    var iduser: String? = null,
     var idusertitip: String? = null,
     var confiados: String? = null,
+    var user: UserModel? = null,
     var usertitip: UserModel? = null
 )
